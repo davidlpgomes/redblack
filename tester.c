a1b9871de6dca504dc21f51d02c77e94ca0a13f5
David Gomes <david.gomes@tuta.io>
2023-04-17 21:39:48 -0300

    Add tester

---
+++ tester.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tree.h"
+
+int main() {
+    tree_t* tree = tree_create();
+
+    tree_node_t* p1 = tree_insert(tree, 0, NULL);
+    print_tree(tree);
+
+    tree_node_t* p2 = tree_insert(tree, 10, NULL);
+    print_tree(tree);
+
+    tree_node_t* p3 = tree_insert(tree, 5, NULL);
+    print_tree(tree);
+    
+    tree_node_t* p4 = tree_insert(tree, 8, NULL);
+    print_tree(tree);
+
+    tree_node_t* p5 = tree_insert(tree, 2, NULL);
+    print_tree(tree);
+
+    tree_delete(tree, p3);
+    print_tree(tree);
+
+    tree_delete(tree, p1);
+    print_tree(tree);
+
+    tree_delete(tree, p2);
+    print_tree(tree);
+
+    tree_free(tree);
+
+    return 0;
+}
